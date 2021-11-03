5a5c9a1776af17c03c656eda1b82d0443f439688
Gitthuma <george.githuma@gmail.com>
2021-11-03 23:15:41 +0300

    Use if statement to check if op is addition and print addition of num1 and num2

--- app.py
+++ app.py
@@ -3,3 +3,7 @@
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 op = input("Enter operator: ")
+
+# Use if statement to check if op is addition and print addition of num1 and num2
+if op == '+':
+    print("The addition is:", num1 + num2)

7dc9097f60a6cc20cf521658832d23468fc93fc4
Gitthuma <george.githuma@gmail.com>
2021-11-03 23:05:39 +0300

    Create three inputs, two numbers and an operator and store them in variables

---
+++ app.py
@@ -0,0 +1,4 @@
+# Create three inputs, two numbers and an operator and store them in variables
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+op = input("Enter operator: ")
